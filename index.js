39fd4756fe5577221289726b61cc035f49bdff32
Deadcoder <139893905+Deadpoolhp-4@users.noreply.github.com>
2024-05-15 14:17:55 +0530

    new

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./server.js');
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running on http://localhost:${port}`);
 });
